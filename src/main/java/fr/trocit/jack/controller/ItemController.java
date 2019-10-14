faa97566cd7c0c74e00d49c811b258e3134dbd27
abensaid <arthur.bensaid@gmail.com>
2019-10-14 13:45:10 +0200

    Cross Origin request

--- src/main/java/fr/trocit/jack/controller/ItemController.java
+++ src/main/java/fr/trocit/jack/controller/ItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import fr.trocit.jack.repository.AbstractItemRepository;
 import fr.trocit.jack.service.ItemService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("users/{usrId}/items")
 public class ItemController {
 	
